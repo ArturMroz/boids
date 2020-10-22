2c12762e35f56fc3b55ec5a2a6f0f2730fa63aec
Artur Mroz <frost47@gmail.com>
2020-10-22 21:56:38 +0100

    change to Data Oriented Design

--- boids.js
+++ boids.js
@@ -6,52 +6,42 @@ canvas.height = window.innerHeight;
 ctx.fillStyle = '#302e21';
 
 const centeringFactor = 0.005;
-const avoidFactor = 0.04;
-const matchVelocityFactor = 0.07;
-const turnAwayFromBoundsFactor = 1;
+const avoidOthersFactor = 0.06;
+const matchVelocityFactor = 0.08;
+const avoidWallsFactor = 0.8;
 
 const avoidDistance = 25;
-const visualRange = 160;
-const boundsMargin = 200;
+const visualRange = 170;
+const boundsMargin = 180;
+const speedLimit = 16;
 
-const speedLimit = 14;
+const boidsNum = 700;
 
-const boidsNum = 500;
-const boids = new Array(boidsNum);
+// allocate one buffer and divide it into 4 for float arrays
+const buf = new ArrayBuffer(boidsNum * 4 * Float32Array.BYTES_PER_ELEMENT);
+// horizontal positions
+const xs = new Float32Array(buf, (0 * boidsNum) * Float32Array.BYTES_PER_ELEMENT, boidsNum);
+// vertical positions
+const ys = new Float32Array(buf, (1 * boidsNum) * Float32Array.BYTES_PER_ELEMENT, boidsNum);
+// horizontal velocities
+const vxs = new Float32Array(buf, (2 * boidsNum) * Float32Array.BYTES_PER_ELEMENT, boidsNum);
+// vertical velocities
+const vys = new Float32Array(buf, (3 * boidsNum) * Float32Array.BYTES_PER_ELEMENT, boidsNum);
 
-for (let i = 0; i < boids.length; i++) {
-    boids[i] = {
-        x: Math.floor(Math.random() * canvas.width),
-        y: Math.floor(Math.random() * canvas.height),
-        xv: Math.random() * 10 - 5,
-        yv: Math.random() * 10 - 5,
-    }
-}
-
-function keepWithinScreenBounds(boid) {
-    if (boid.x < boundsMargin) boid.xv += turnAwayFromBoundsFactor;
-    if (boid.y < boundsMargin) boid.yv += turnAwayFromBoundsFactor;
-    if (boid.x > canvas.width - boundsMargin) boid.xv -= turnAwayFromBoundsFactor;
-    if (boid.y > canvas.height - boundsMargin) boid.yv -= turnAwayFromBoundsFactor;
-}
-
-function limitSpeed(boid) {
-    const speed = Math.sqrt(boid.xv * boid.xv + boid.yv * boid.yv);
-
-    if (speed > speedLimit) {
-        boid.xv = (boid.xv / speed) * speedLimit;
-        boid.yv = (boid.yv / speed) * speedLimit;
-    }
+for (let i = 0; i < boidsNum; i++) {
+    xs[i] = Math.floor(Math.random() * canvas.width);
+    ys[i] = Math.floor(Math.random() * canvas.height);
+    vxs[i] = Math.random() * 10 - 5;
+    vys[i] = Math.random() * 10 - 5;
 }
 
 // declare variables here and reuse them to avoid GC
-
 let centreOfMassX, centreOfMassY;
 let avoidOthersX, avoidOthersY;
 let avgVelocityX, avgVelocityY;
 let numberOfNeighbours;
 let i, j;
-let distance;
+let distance, speed;
 
 function draw() {
     requestAnimationFrame(draw)
@@ -71,48 +61,62 @@ function draw() {
         numberOfNeighbours = 0;
 
         for (j = 0; j < boidsNum; j++) {
-            distance = Math.sqrt((boids[i].x - boids[j].x) ** 2 + (boids[i].y - boids[j].y) ** 2);
+            // distance = Math.sqrt((xs[i] - xs[j]) ** 2 + (ys[i] - ys[j]) ** 2);
+            // simplified distance calculation
+            distance = Math.abs(xs[i] - xs[j]) + Math.abs(ys[i] - ys[j])
 
             if (distance > visualRange) continue;
 
-            centreOfMassX += boids[j].x;
-            centreOfMassY += boids[j].y;
+            centreOfMassX += xs[j];
+            centreOfMassY += ys[j];
 
-            avgVelocityX += boids[j].xv;
-            avgVelocityY += boids[j].yv;
+            avgVelocityX += vxs[j];
+            avgVelocityY += vys[j];
 
             if (distance < avoidDistance) {
-                avoidOthersX += boids[i].x - boids[j].x;
-                avoidOthersY += boids[i].y - boids[j].y;
+                avoidOthersX += xs[i] - xs[j];
+                avoidOthersY += ys[i] - ys[j];
             }
 
             numberOfNeighbours++;
         }
 
         // Rule 1: Boids try to fly towards the centre of mass
-        centreOfMassX = (centreOfMassX / numberOfNeighbours) - boids[i].x;
-        centreOfMassY = (centreOfMassY / numberOfNeighbours) - boids[i].y;
-        boids[i].xv += centreOfMassX * centeringFactor;
-        boids[i].yv += centreOfMassY * centeringFactor;
+        centreOfMassX = (centreOfMassX / numberOfNeighbours) - xs[i];
+        centreOfMassY = (centreOfMassY / numberOfNeighbours) - ys[i];
+        vxs[i] += centreOfMassX * centeringFactor;
+        vys[i] += centreOfMassY * centeringFactor;
 
         // Rule 2: Boids try to keep a small distance away from other boids
-        boids[i].xv +=  avoidOthersX * avoidFactor;
-        boids[i].yv += avoidOthersY * avoidFactor;
+        vxs[i] +=  avoidOthersX * avoidOthersFactor;
+        vys[i] += avoidOthersY * avoidOthersFactor;
 
-        // Rule 3: Boids try to match velocity with near boids
+        // Rule 3: Boids try to match velocity with other boids
         avgVelocityX = avgVelocityX / numberOfNeighbours;
         avgVelocityY = avgVelocityY / numberOfNeighbours;
-        boids[i].xv += avgVelocityX * matchVelocityFactor;
-        boids[i].yv += avgVelocityY * matchVelocityFactor;
+        vxs[i] += avgVelocityX * matchVelocityFactor;
+        vys[i] += avgVelocityY * matchVelocityFactor;
 
-        limitSpeed(boids[i]);
-        keepWithinScreenBounds(boids[i]);
+        // limit speed
+        // speed = Math.sqrt(vxs[i] * vxs[i] + vys[i] ** 2 );
+        speed = Math.abs(vxs[i]) + Math.abs(vys[i]); // simplified speed estimation
+
+        if (speed > speedLimit) {
+            vxs[i] = (vxs[i] / speed) * speedLimit;
+            vys[i] = (vys[i] / speed) * speedLimit;
+        }
+
+        // steer away from the screen bounds
+        if (xs[i] < boundsMargin) vxs[i] += avoidWallsFactor;
+        if (ys[i] < boundsMargin) vys[i] += avoidWallsFactor;
+        if (xs[i] > canvas.width - boundsMargin) vxs[i] -= avoidWallsFactor;
+        if (ys[i] > canvas.height - boundsMargin) vys[i] -= avoidWallsFactor;
 
         // round it so anti-aliasing doesn't kick in
-        boids[i].x = (boids[i].x + boids[i].xv + 0.5) >> 0;
-        boids[i].y = (boids[i].y + boids[i].yv + 0.5) >> 0;
+        xs[i] = (xs[i] + vxs[i] + 0.5) >> 0;
+        ys[i] = (ys[i] + vys[i] + 0.5) >> 0;
 
-        ctx.fillRect(boids[i].x, boids[i].y, 6, 6);
+        ctx.fillRect(xs[i], ys[i], 5, 5);
     }
 }
 
--- readme.md
+++ readme.md
@@ -2,4 +2,6 @@
 
 Flocking simulator inspired by [this video](https://www.youtube.com/watch?v=4LWmRuB-uNU). Based on Reynolds' algorithm, more info [here](http://www.kfish.org/boids/pseudocode.html). 
 
-See it live [here »](https://arturmroz.github.io/boids/)
+Using Data Oriented Design to improve performance.
+
+[See it live here »](https://arturmroz.github.io/boids/)
